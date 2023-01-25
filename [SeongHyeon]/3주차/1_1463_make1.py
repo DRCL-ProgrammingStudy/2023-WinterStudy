c4188cad47a110555654c471f6a87d68cd2176dc
Kim Seong Hyeon <dhkdwk0215@naver.com>
2023-01-25 22:55:23 +0900

    Update 1_1463_make1.py

--- [SeongHyeon]/3주차/1_1463_make1.py
+++ [SeongHyeon]/3주차/1_1463_make1.py
@@ -2,16 +2,11 @@ import sys
 
 
 N = int(sys.stdin.readline())
-count = 0
-
-while N != 1:
-    if (N % 3)== 0:
-        N //=3
-        count +=1
-    if (N % 2) == 0:
-        N //= 2
-        count +=1
-    else:
-        N-=1
-        count +=1
-print(count)
+d = [0] *(N+1)
+for i in range(2,N+1): 
+    d[i]=d[i-1]+1 
+    if i%2==0: 
+        d[i]=min(d[i],d[i//2]+1)
+    if i%3==0: 
+        d[i]=min(d[i],d[i//3]+1)
+print(d[N])
