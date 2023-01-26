10ca1795ae8a3ac675c171be949946d05988ef64
Topasm <dhkdwk0215@naver.com>
2023-01-26 10:36:25 +0900

    fdg

--- [SeongHyeon]/3주차/1_1463_make1.py
+++ [SeongHyeon]/3주차/1_1463_make1.py
@@ -2,16 +2,11 @@ import sys
 
 
 N = int(sys.stdin.readline())
-count = 0
-
-while N != 1:
-    if (N % 3)== 0:
-        N //=3
-        count +=1
-    if (N % 2) == 0:
-        N //= 2
-        count +=1
-    else:
-        N-=1
-        count +=1
-print(count)
+d = [0] *(N+1)
+for i in range(2,N+1): # 2부터 x까지 반복
+    d[i]=d[i-1]+1 # 1을 빼는 연산 -> 1회 진행
+    if i%2==0: # 2로 나누어 떨어질 때, 2로 나누는 연산
+        d[i]=min(d[i],d[i//2]+1)
+    if i%3==0: # 3으로 나누어 떨어질 때, 3으로 나누는 연산
+        d[i]=min(d[i],d[i//3]+1)
+print(d[N])
