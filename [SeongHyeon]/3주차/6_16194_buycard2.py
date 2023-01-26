d4bb650ee7979b2a7d1906bac0295582442695ef
Topasm <dhkdwk0215@naver.com>
2023-01-26 16:32:32 +0900

    bc

--- [SeongHyeon]/3주차/6_16194_buycard2.py
+++ [SeongHyeon]/3주차/6_16194_buycard2.py
@@ -4,10 +4,10 @@ import sys
 N = int(sys.stdin.readline())
 
 com = [0] + list(sys.stdin.readline().split())
-dp = [0]*(N+1)
-
+dp = [9999999999999999999999999999]*(N+1)
+dp[0] = 0
 
 for i in range(1,N+1):
     for k in range(1,i+1):
-        dp[i] = max(dp[i], dp[i-k] + int(com[k]))
-print(dp[i])
+        dp[i] = min(dp[i], dp[i-k] + int(com[k]))
+print(dp[N])
